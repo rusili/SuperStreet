95fa9a5f91f30d8086fd448114307c8d9f511ada
rusili <>
2018-06-16 16:22:01 -0400

    cleaned up unused gradle plugins. set image activity background to black

---
+++ app/src/main/java/com/rusili/superstreet/ui/util/SuperGlideModule.java
@@ -0,0 +1,8 @@
+package com.rusili.superstreet.ui.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class SuperGlideModule extends AppGlideModule {
+}
