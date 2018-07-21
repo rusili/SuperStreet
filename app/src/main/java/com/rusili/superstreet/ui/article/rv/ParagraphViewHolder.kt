296cf2c8a83a0bf69bb463a55258a8b3c401a7a0
rusili <>
2018-07-20 21:29:12 -0400

    text_future Added text future for paragraph view

--- app/src/main/java/com/rusili/superstreet/ui/article/rv/ParagraphViewHolder.kt
+++ app/src/main/java/com/rusili/superstreet/ui/article/rv/ParagraphViewHolder.kt
@@ -1,6 +1,8 @@
 package com.rusili.superstreet.ui.article.rv
 
 import android.view.View
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
 import com.rusili.superstreet.domain.models.body.Paragraph
 import com.rusili.superstreet.ui.common.BaseViewHolder
 import kotlinx.android.extensions.LayoutContainer
@@ -10,6 +12,8 @@ class ParagraphViewHolder(override val containerView: View)
     : BaseViewHolder<Paragraph>(containerView), LayoutContainer {
 
     override fun bind(model: Paragraph) {
-        articleParagraph.text = model.body
+        val textFuture = PrecomputedTextCompat.getTextFuture(model.body,
+                TextViewCompat.getTextMetricsParams(articleParagraph), null)
+        articleParagraph.setTextFuture(textFuture)
     }
 }
