08f2cd65f2dc01eed049a926104c9d69adf8e6e6
rusili <>
2018-07-21 23:32:39 -0400

    fix-timestamp_today timestamp now displays today and yesterday for 0 and 1 days ago.

--- app/src/main/java/com/rusili/superstreet/ui/list/di/PreviewListModule.kt
+++ app/src/main/java/com/rusili/superstreet/ui/list/di/PreviewListModule.kt
@@ -3,11 +3,16 @@ package com.rusili.superstreet.ui.list.di
 import com.rusili.superstreet.domain.list.ArticleListDataSourceFactory
 import com.rusili.superstreet.domain.list.ArticleListRepository
 import com.rusili.superstreet.domain.list.ArticleListUsecase
+import com.rusili.superstreet.ui.util.DateHelper
 import dagger.Module
 import dagger.Provides
 
 @Module
 class PreviewListModule() {
+
+    @Provides
+    fun provideDateHelper() =
+            DateHelper()
 
     @Provides
     fun provideArticleListViewModelFactory(dataSourceFactory: ArticleListDataSourceFactory) =
