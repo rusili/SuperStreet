8bce08f71abf0fa635777d7be74f1062fb270745
rusili <rusili56@gmail.com>
2019-05-12 16:08:36 -0400

    fix/image_url imageurl is now parcelable

--- app/src/main/java/com/rusili/superstreet/common/models/ImageUrl.kt
+++ app/src/main/java/com/rusili/superstreet/common/models/ImageUrl.kt
@@ -1,12 +1,17 @@
 package com.rusili.superstreet.common.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 private const val URL_QUERY_WIDTH = "width="
 private const val URL_QUERY_HEIGHT = "height="
 
 private const val URL_QUERY_CHAR = '?'
 private const val URL_AND_CHAR = '&'
 
-inline class ImageUrl(val url: String) {
+// TODO: Convert to inline class when Parcelable is supported
+@Parcelize
+data class ImageUrl(val url: String) : Parcelable {
 
     // Default Image width & height is 660 x 440
     fun resize(
